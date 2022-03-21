3e9f0e06069bbfdc3e985649330a463febefbbc4
yuhongbo <YHB@DESKTOP-7986EO4>
2022-03-21 09:18:32 +0800

    初始化项目

---
+++ src/components/MyHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ store.count }}</h1>
+  <button @click="linkTo">to about</button>
+</template>
+
+<script setup lang="ts">
+// import { Router, useRouter } from 'vue-router'
+import { useCounterStore } from '../store/store'
+
+// const router: Router = useRouter()
+const linkTo = () => {
+  store.increment()
+  // router.push({
+  //   path: '/about'
+  // })
+}
+
+const store = useCounterStore()
+</script>
+
+<style scoped></style>
